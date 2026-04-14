c97c4bdd70702feac97f005e065725efd13d974a
Ayuningtyas24 <adyahseptiani@gmail.com>
2026-04-14 11:55:45 +0700

    menambahkan fungsi ClearForm untuk mengosongkan inputan

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -233,4 +233,14 @@ namespace CRUDMahasiswaADO
             }
         }
 
-       
+        private void ClearForm()
+        {
+            txtNIM.Clear();
+            txtNama.Clear();
+            cmbJK.SelectedIndex = -1;
+            dtpTanggalLahir.Value = DateTime.Now;
+            txtAlamat.Clear();
+            txtKodeProdi.Clear();
+        }
+
+        
